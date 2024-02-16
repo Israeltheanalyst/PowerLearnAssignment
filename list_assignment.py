10660278d5b3f9ba8b969b2ae9ea4579c14611b0
Israeltheanalyst <israelolasupo35@gmail.com>
2024-02-16 15:34:09 +0100

    Python list

---
+++ list_assignment.py
@@ -0,0 +1,23 @@
+# Create an empty list called my_list
+my_list = []
+
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+# Insert the value 15 at the second position in the list
+my_list.insert(1, 15)
+
+
+my_list.extend([50, 60, 70])
+
+# Remove the last element from my_list
+my_list.pop()
+
+# Sort my_list in ascending order
+my_list.sort()
+
+# Find and print the index of the value 30 in my_list
+index_30 = my_list.index(30)
+print("Index of 30 in my_list:", index_30)
